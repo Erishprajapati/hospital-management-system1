840c88dc15febc0ee2dcf6070e4d6d1149565f7e
Erish Prajapati <irishmjn@gmail.com>
2025-04-28 22:01:03 +0545

    admin registered

--- mainfolder/codes/admin.py
+++ mainfolder/codes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Billing)
+admin.site.register(Appointment)
+admin.site.register(Room)
+admin.site.register(Admission)
+admin.site.register(Prescription)
+admin.site.register(Department)
+
