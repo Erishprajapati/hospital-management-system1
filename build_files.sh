3bea4cd21dd1447731e319d0d15a31b2f82d8771
Erishprajapati <irishmjn@gmail.com>
2025-05-16 23:01:17 +0545

    changes

--- build_files.sh
+++ build_files.sh
@@ -3,13 +3,13 @@
 echo "BUILD START"
 
 # Install Python dependencies
-python3.9 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Create staticfiles_build directory if it doesn't exist
 mkdir -p staticfiles_build/public
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 # Move static files to the correct directory
 cp -r staticfiles_build/static/* staticfiles_build/public/
